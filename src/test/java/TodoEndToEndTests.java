a76e6b5e8adcd44a61b574a207d87ab14a626ae2
Aurika <ponylfn@gmail.com>
2022-12-23 02:37:37 +0200

    refactored after video

--- src/test/java/TodoEndToEndTests.java
+++ src/test/java/TodoEndToEndTests.java
@@ -2,13 +2,15 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TodoEndToEndTests {
 
     @Test
-    public void crudEndToEndTestCase() {
+    public void todosLifeCycle() {
         open("http://todomvc4tasj.herokuapp.com/");
         Selenide.Wait().until(ExpectedConditions.jsReturnsValue(
                 "return Object.keys(require.s.contexts._.defined).length === 39;"));
@@ -16,29 +18,28 @@ public class TodoEndToEndTests {
         $("#new-todo").setValue("a").pressEnter();
         $("#new-todo").setValue("b").pressEnter();
         $("#new-todo").setValue("c").pressEnter();
-        $("#new-todo").setValue("d").pressEnter();
-        $$("#todo-list li").shouldHave(exactTexts("a", "b", "c", "d"));
-        $$("#todo-list>li").shouldHave(size(4));
+        $$("#todo-list li").shouldHave(exactTexts("a", "b", "c"));
 
-        $("#todo-list li:nth-of-type(2) .toggle").click();
-        $$("#todo-list li.completed")
-                .shouldHave(exactTexts("b"));
-        $$("#todo-list>li:not(.completed)")
-                .shouldHave(exactTexts("a", "c", "d"));
+        $$("#todo-list>li").findBy(exactText("b")).doubleClick();
+        $$("#todo-list>li").findBy(cssClass("editing"))
+                .find(" .edit")
+                .append(" edited").pressEnter();
 
-
-        $("#todo-list li:nth-of-type(4)").hover();
-        $("#todo-list li:nth-of-type(4) .destroy").click();
-        $$("#todo-list>li").shouldHave(exactTexts("a", "b", "c"));
-
-        $("#filters > li:nth-child(2)").click();
-        $$("#todo-list li.active").shouldHave(exactTexts("a", "c"));
-
-        $("#filters > li:nth-child(3)").click();
-        $$("#todo-list li.completed").shouldHave(exactTexts("b"));
-
+        $$("#todo-list>li").findBy(exactText("b edited"))
+                .find(" .toggle").click();
         $("#clear-completed").click();
-        $$("#todo-list li.completed").shouldBe(empty);
+        $$("#todo-list>li").shouldBe(exactTexts("a","c"));
+
+        $$("#todo-list>li").findBy(exactText("c")).doubleClick();
+        $$("#todo-list>li").findBy(cssClass("editing"))
+                .find(" .edit")
+                .append(" to be canceled").pressEscape();
+
+        $$("#todo-list>li").findBy(exactText("c")).hover()
+                .find(".destroy").click();
+        $$("#todo-list>li").shouldHave(exactTexts("a"));
+
+
     }
 
 }
