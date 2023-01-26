e3e5f5e69644bdf7b5cee8069636294dad762fb1
Aurika <ponylfn@gmail.com>
2023-01-26 22:37:16 +0200

    first version of KISS principle refactoring

--- src/test/java/TodoEndToEndTests.java
+++ src/test/java/TodoEndToEndTests.java
@@ -3,43 +3,68 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
-import static com.codeborne.selenide.Condition.cssClass;
-import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TodoEndToEndTests {
 
     @Test
     public void todosLifeCycle() {
+        openTodoPage();
+        addTodo("a");
+        addTodo("b");
+        addTodo("c");
+        verifyTodosDisplayed("a", "b", "c");
+        editTodoWithText("b", " edited");
+        completeTodo("b edited");
+        clearCompletedTodos();
+        verifyTodosDisplayed("a", "c");
+        cancelEditTodo("c", " to be canceled");
+        deleteTodo("c");
+        verifyTodosDisplayed("a");
+    }
+
+
+    private void openTodoPage() {
         open("http://todomvc4tasj.herokuapp.com/");
         Selenide.Wait().until(ExpectedConditions.jsReturnsValue(
                 "return Object.keys(require.s.contexts._.defined).length === 39;"));
+    }
 
-        $("#new-todo").setValue("a").pressEnter();
-        $("#new-todo").setValue("b").pressEnter();
-        $("#new-todo").setValue("c").pressEnter();
-        $$("#todo-list li").shouldHave(exactTexts("a", "b", "c"));
+    private void addTodo(String text) {
+        $("#new-todo").setValue(text).pressEnter();
+    }
 
-        $$("#todo-list li").findBy(exactText("b")).doubleClick();
+    private void verifyTodosDisplayed(String... todoTexts) {
+        $$("#todo-list li").shouldHave(exactTexts(todoTexts));
+    }
+
+    private void editTodoWithText(String originalText, String editedText) {
+        $$("#todo-list li").findBy(exactText(originalText)).doubleClick();
         $$("#todo-list li").findBy(cssClass("editing"))
                 .find(" .edit")
-                .append(" edited").pressEnter();
+                .append(editedText).pressEnter();
+    }
 
-        $$("#todo-list li").findBy(exactText("b edited"))
+    private void completeTodo(String text) {
+        $$("#todo-list li").findBy(exactText(text))
                 .find(" .toggle").click();
-        $("#clear-completed").click();
-        $$("#todo-list li").shouldHave(exactTexts("a","c"));
-
-        $$("#todo-list li").findBy(exactText("c")).doubleClick();
-        $$("#todo-list li").findBy(cssClass("editing"))
-                .find(" .edit")
-                .append(" to be canceled").pressEscape();
-
-        $$("#todo-list li").findBy(exactText("c")).hover()
-                .find(".destroy").click();
-        $$("#todo-list li").shouldHave(exactTexts("a"));
-
 
     }
 
+    private void clearCompletedTodos() {
+        $("#clear-completed").click();
+    }
+
+    private void cancelEditTodo(String originalText, String editedText) {
+        $$("#todo-list li").findBy(exactText(originalText)).doubleClick();
+        $$("#todo-list li").findBy(cssClass("editing"))
+                .find(" .edit")
+                .append(editedText).pressEscape();
+    }
+
+    private void deleteTodo(String text) {
+        $$("#todo-list li").findBy(exactText(text)).hover()
+                .find(".destroy").click();
+    }
 }
