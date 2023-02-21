64354a37f67e5795a7ab38aae4b1c376046c7e96
Aurika <ponylfn@gmail.com>
2023-02-21 23:59:20 +0200

    added default value Configuration.baseUrl

--- src/test/java/com/taotas/todomvctest/BaseTest.java
+++ src/test/java/com/taotas/todomvctest/BaseTest.java
@@ -2,9 +2,12 @@ package com.taotas.todomvctest;
 
 import com.codeborne.selenide.Configuration;
 
+import static java.lang.System.getProperty;
+
 public class BaseTest {
     {
         Configuration.fastSetValue = true;
-        Configuration.baseUrl = System.getProperty("baseUrl", "http://todomvc4tasj.herokuapp.com");
+        Configuration.baseUrl =  getProperty(
+                "selenide.baseUrl", "http://todomvc4tasj.herokuapp.com");
     }
 }
